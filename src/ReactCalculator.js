2e13396c79ce7999eb8f35e5efee68f8f152205b
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-03 12:19:51 -0300

    [Style] Create input buttons (still no listening to touch events)

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,21 +1,59 @@
+// React imports
 import React, { Component } from 'react'
-import Style from './Style'
 import {
-    View,
-    AppRegistry
+  View,
+  AppRegistry
 } from 'react-native'
+
+// Component imports
+import Style from './Style'
+import InputButton from './InputButton'
+
+// Define buttons
+const inputButtons = [
+  [1, 2, 3, '/'],
+  [4, 5, 6, '*'],
+  [7, 8, 9, '-'],
+  [0, '.', '=', '+']
+]
 
 class ReactCalculator extends Component {
 
-    render() {
-        return (
-          <View style={Style.rootContainer}>
-              <View style={Style.displayContainer}></View>
-              <View style={Style.inputContainer}></View>
-          </View>
+  render() {
+    return (
+      <View style={Style.rootContainer}>
+        <View style={Style.displayContainer}></View>
+        <View style={Style.inputContainer}>
+          {this._renderInputButtons()}
+        </View>
+      </View>
+    )
+  }
+
+
+  // For each row in `inputButtons`, create a row View and add create an InputButton for each input in the row.
+  _renderInputButtons() {
+    let views = []
+
+    for (var r = 0; r < inputButtons.length; r ++) {
+      let row = inputButtons[r]
+
+      let inputRow = []
+      for (var i = 0; i < row.length; i ++) {
+        let input = row[i]
+
+        inputRow.push(
+          <InputButton value={input} key={r + "-" + i} />
         )
+      }
+
+      views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
     }
+
+    return views
+  }
 
 }
 
+// Register default Component which should be rendered first
 AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator)
